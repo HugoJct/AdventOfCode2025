abbf700d31e90a78a901604adcb6d446eed59c53
Hugo Jacotot <hjacotot@protonmail.com>
2025-12-01 13:05:43 +0100

    day 1 part 2

--- day1/bin/main.ml
+++ day1/bin/main.ml
@@ -15,25 +15,34 @@ let modulo x y =
     then modi + y 
   else modi
 
-let update_dial dial (line: string) =
-  let parts = 
+let update_dial dial acc (line: string) =
+  let rec parts = 
     match (List.of_seq (String.to_seq line)) with
     | e::l -> (e, (String.of_seq (List.to_seq l)))
-    | _ -> failwith "wrong format" in
+    | _ -> failwith "wrong format"
+  and loop dial acc offset = 
+    (* check on offset different from zero to avoid counting it twice 
+       when dial = offset = 0
+     * - once at the last loop iteration 
+     * - once at the first iteration of the next loop
+     *)
+    let new_acc = if dial = 0 && offset <> 0 then (acc+1) else acc in
+    if offset > 0 then loop (modulo (dial + 1) 100) new_acc (offset - 1)
+    else if offset < 0 then loop (modulo (dial - 1) 100) new_acc (offset + 1)
+    else (dial, (new_acc)) in 
   if (fst parts) = 'L' 
-    then (modulo (dial - (int_of_string (snd parts))) 100) 
+    then loop dial acc (-(int_of_string (snd parts)))
   else if (fst parts) = 'R' 
-    then (modulo (dial + (int_of_string (snd parts))) 100)
+    then loop dial acc (int_of_string (snd parts))
   else failwith "wrong direction"
 
 let apply_instructions lines =
   let rec loop l dial acc =
   match l with 
   | e::l' -> (
-        let new_dial = (update_dial dial e) in 
-        if new_dial = 0 
-          then (loop l' new_dial (acc + 1))
-          else (loop l' new_dial acc)
+        let updated = (update_dial dial acc e) in 
+        let new_dial = (fst updated) and new_acc = (snd updated) in
+          loop l' new_dial new_acc
       )
   | [] -> acc in 
   loop lines 50 0;;
