f488cd5af4b3407395d0a409b4d5eddb055e75da
Hugo Jacotot <hjacotot@protonmail.com>
2025-12-03 22:08:46 +0100

    day 3 part 2

--- day3/bin/main.ml
+++ day3/bin/main.ml
@@ -9,18 +9,14 @@ let parse_file filepath =
     | None -> List.rev acc in
   get_lines [];;
 
-let max_first bank =
+let max bank digits_left =
   let rec aux bank max =
     match bank with
-    | e1::_::[] -> if e1 > max then e1 else max
-    | e::l -> if e > max then (aux l e) else (aux l max)
-    | [] -> max in
-  aux bank 0;;
-
-let max_second bank =
-  let rec aux bank max =
-    match bank with
-    | e::l -> if e > max then (aux l e) else (aux l max)
+    | e::l -> if (List.length l) < digits_left 
+      then 
+        (if e > max then e else max)
+      else 
+        (if e > max then (aux l e) else (aux l max))
     | [] -> max in
   aux bank 0;;
 
@@ -40,15 +36,23 @@ let sublist start list =
   aux 0 list [];;
 
 let process_line line = 
-  let bank = (line_to_list line) in 
-  let first = (max_first bank) in 
-  let index = 
-    match (List.find_index (fun x -> x = first) bank) with 
-    | Some(i) -> i
-    | _ -> failwith "no index" in 
-  let second = (max_second (sublist index bank)) in
-  (int_of_string (String.of_seq (List.to_seq (List.map (fun x -> (char_of_int x))(first::second::[])))));;
+  let rec aux newbank i acc =
+    if i <= 0 then (List.rev acc) else 
+    let newdigit = (max newbank i) in 
+    let index = 
+      match (List.find_index (fun x -> x = newdigit) newbank) with 
+      | Some(index) -> index
+      | _ -> failwith "no index" in 
+    aux (sublist index newbank) (i-1) (newdigit::acc) and
+    bank = (line_to_list line) in
+  let value_as_list = aux bank 12 [] in 
+  (int_of_string (String.of_seq (List.to_seq (List.map (fun x -> (char_of_int x)) (value_as_list)))));;
+
+let rec process_lines lines = 
+  match lines with 
+  | e::l -> (process_line e)::(process_lines l)
+  | [] -> []
 
 let () = 
   let input = (parse_file "res/input.txt") in 
-  print_endline (string_of_int (List.fold_left (fun acc x -> acc + (process_line x)) 0 input));;
+  print_int (List.fold_left (fun acc x -> acc + x) 0 (process_lines input));
