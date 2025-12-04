3674b8bb86a1aac7d0f3b9db33adc6d43ff78a5f
Hugo Jacotot <hjacotot@protonmail.com>
2025-12-04 21:22:46 +0100

    day 4 part 2

--- day4/bin/main.ml
+++ day4/bin/main.ml
@@ -22,22 +22,22 @@ let check_spot x y arr =
     do begin
       for j = (-1) to 1
         do begin
-          if i = 0 && j = 0 
-            then ()
+          if i = 0 && j = 0 then ()
           else if arr.(x+i).(y+j) = '@' 
-            then count := (!count + 1)
+            then count := !count + 1
         end done
     end done ; 
-  (!count < 4);;
+  count;;
 
 let process arr = 
-  let count = ref 0 in
+  let count = ref [] in
   for i = 1 to (Array.length arr) - 2
     do begin
       for j = 1 to (Array.length arr) - 2
         do begin
           if arr.(i).(j) <> '@' then ()
-          else if (check_spot i j arr) then count := (!count + 1)
+          else if !(check_spot i j arr) < 4 
+            then count := ((i, j))::(!count)
         end done
     end done ;
   (!count);;
@@ -45,4 +45,12 @@ let process arr =
 let () = 
   let input = (parse_file "res/input.txt") in
   let arr = (list_to_array input) in 
-  print_int (process arr) ; print_newline ();;
+  let count = ref 0 in
+  let cur = ref ((0,0)::[]) in 
+  while !cur |> List.length <> 0
+    do begin
+      cur := (process arr) ;
+      count := !count + (!cur |> List.length);
+      (List.iter (fun (x, y) -> arr.(x).(y) <- '.') !cur) 
+    end done ;
+  print_int !count ; print_newline ();;
